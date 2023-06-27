614cca15a631a2181adf611bb173df680b6a33e8
Evangeline Wong <wong.evangeline77@gmail.com>
2023-06-27 15:02:45 -0400

    creating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', 
+  template: `<p>Hello World</p>`
 })
+
 export class AppComponent {
-  title = 'basics';
+
 }
